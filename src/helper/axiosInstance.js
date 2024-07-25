a01747a10489244964e0867daedbf044f57c0bb9
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-25 10:54:09 +0530

    update env

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+	baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`,
 	// timeout: 10000,
 	headers: { "Content-Type": "application/json" },
 	withCredentials: true,
