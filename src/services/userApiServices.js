f1803e13dbd6f323245987ac7188ff4acfe2c0bb
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-25 12:50:31 +0530

    userApiService - remove header

--- src/services/userApiServices.js
+++ src/services/userApiServices.js
@@ -3,22 +3,12 @@ import axiosInstance from "@/helper/axiosInstance";
 class authService {
 	signupUser = async (data) => await axiosInstance.post("/users", data);
 
-	loginUser = async (data) =>
-		await axiosInstance.post("/users/login", data, {
-			headers: {
-				Cookie: context.req.headers.cookie || "",
-			},
-		});
+	loginUser = async (data) => await axiosInstance.post("/users/login", data);
 
 	changePassword = async (data) =>
 		await axiosInstance.post("/users/change-password", data);
 
-	logoutUser = async () =>
-		await axiosInstance.post("/users/logout", {
-			headers: {
-				Cookie: context.req.headers.cookie || "", // Pass cookies from server to the API request
-			},
-		});
+	logoutUser = async () => await axiosInstance.post("/users/logout");
 
 	getUser = async () => await axiosInstance.get("/users");
 
