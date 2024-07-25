3779243e1612cc6b265894bedb7fe9696fbc911e
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-25 12:47:49 +0530

    userApiService - add axios headers cookies

--- src/services/userApiServices.js
+++ src/services/userApiServices.js
@@ -3,12 +3,22 @@ import axiosInstance from "@/helper/axiosInstance";
 class authService {
 	signupUser = async (data) => await axiosInstance.post("/users", data);
 
-	loginUser = async (data) => await axiosInstance.post("/users/login", data);
+	loginUser = async (data) =>
+		await axiosInstance.post("/users/login", data, {
+			headers: {
+				Cookie: context.req.headers.cookie || "",
+			},
+		});
 
 	changePassword = async (data) =>
 		await axiosInstance.post("/users/change-password", data);
 
-	logoutUser = async () => await axiosInstance.post("/users/logout");
+	logoutUser = async () =>
+		await axiosInstance.post("/users/logout", {
+			headers: {
+				Cookie: context.req.headers.cookie || "", // Pass cookies from server to the API request
+			},
+		});
 
 	getUser = async () => await axiosInstance.get("/users");
 
