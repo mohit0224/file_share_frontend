1c079fa7ddc4ee6598fe9f4f0f11b250ee2c49a7
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-27 00:24:22 +0530

    header - zindex

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
 
 	return (
 		<>
-			<header className=" fixed top-0 left-0 w-full bg-white z-[999]">
+			<header className=" fixed top-0 left-0 w-full bg-white z-50">
 				<nav className="max-w-7xl mx-auto px-5 py-3 flex justify-between items-center">
 					<div className="logo">
 						<Link href={"/"} className="flex items-end gap-1">
