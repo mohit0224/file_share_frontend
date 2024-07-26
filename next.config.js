87fa8deacecff990c842f93ee6c863aefacfce63
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-26 10:59:45 +0530

    revert

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: `${process.env.BACKEND_URL}`,
-			},
-		];
-	},
 	images: {
 		remotePatterns: [
 			{
