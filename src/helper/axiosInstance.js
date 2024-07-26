8b69cdb58b1e09232ba2939f7fa4e969ed2fcda5
mohit0224 <mohitdheer24+git@gmail.com>
2024-07-26 10:29:05 +0530

    axios instance

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	baseURL: `/api/v1`,
 	withCredentials: true,
+	headers: { "Content-Type": "application/json" },
 	// timeout: 10000,
-	// headers: { "Content-Type": "application/json" },
 });
 
 export default axiosInstance;
